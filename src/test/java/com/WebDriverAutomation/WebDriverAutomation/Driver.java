5c50076882ba760f5525ec0266d2269f84163606
Rafael Carneiro <rafael.carneiro@mereo.com>
2021-06-13 18:35:39 -0300

    Creating Before Method

--- src/test/java/com/WebDriverAutomation/WebDriverAutomation/Driver.java
+++ src/test/java/com/WebDriverAutomation/WebDriverAutomation/Driver.java
@@ -1,6 +1,7 @@
 package com.WebDriverAutomation.WebDriverAutomation;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,16 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Driver {
-
+	@BeforeEach
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver", "D:\\workspace\\conf\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://gitlab.com/elrafapc/webdrivertest");
+	}
 
 	@Test
 	void contextLoads() {
 
-		WebDriver driver;
-
-		System.setProperty("webdriver.chrome.driver", "D:\\workspace\\conf\\ChromeDriver\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://gitlab.com/elrafapc/webdrivertest");
 	}
 
 }
