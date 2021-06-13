ce2026584d3c27f92f1899c14b1a332bb0c0df9b
Rafael Carneiro <rafael.carneiro@mereo.com>
2021-06-13 18:00:44 -0300

    Initializing Project

---
+++ src/main/java/com/WebDriverAutomation/WebDriverAutomation/WebDriverAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.WebDriverAutomation.WebDriverAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDriverAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDriverAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WebDriverAutomation/WebDriverAutomation/WebDriverAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebDriverAutomation.WebDriverAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDriverAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
